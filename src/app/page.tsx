e429e49701602708a9cc4900f9faf60c3ddd4cc2
hayato94087 <hayato94087@gmail.com>
2024-03-29 16:48:51 +0900

    feat:resolveJsonModuleを無効化しエラーを確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { type FC } from "react";
+import settings from "@/lib/settings.json";
 
 const Home: FC = () => {
+  console.log(settings);
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
